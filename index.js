1815af0883cebc7fe124f1667c980a82c9a971ca
MOSS17 <42181277+MOSS17@users.noreply.github.com>
2021-10-05 18:25:43 -0700

    Clase Blockchain

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import PKG from './package.json';
 import Block from './src/blockchain/block'
 
 const { name, version, description, author } = PKG;
+const {genesis} = Block;
 
-console.log(`${name} ${version} ${description} ${author}`);
+const block1 = Block.mine(genesis, 'transac1');
+console.log(block1.toString());
 
-const block = new Block(Date.now(), 'pr3v10us-h4sh', 'h4sh', 'd4t4');
-
-console.log(block.toString());
+const block2 = Block.mine(block1, 'transac2');
+console.log(block2.toString());
